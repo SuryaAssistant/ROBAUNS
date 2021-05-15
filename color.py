86e518996fa33f8aaf57195cd8eb2aacede179dd
SuryaAssistant <09.fandisurya@gmail.com>
2021-05-15 22:10:56 +0700

    color package

---
+++ color.py
@@ -0,0 +1,32 @@
+def color_primary():
+    return (0, 123, 255)
+
+def color_secondary():
+    return (108,117,125)
+
+def color_danger():
+    return (220,53,69)
+
+def color_success():
+    return (40, 167, 69)
+
+def color_warning():
+    return (255, 193, 7)
+
+def color_info():
+    return (23, 162, 184)
+
+def color_light():
+    return (248, 249, 250)
+
+def color_dark():
+    return (52, 58, 64)
+
+def color_white():
+    return (255, 255, 255)
+
+if __name__ == "__main__":
+    print("This is color package based on Bootstrap v4\n")
+    print("Example : \n")
+    print("from color import *")
+    print("object_color = color_success()")
